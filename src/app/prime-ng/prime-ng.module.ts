dc13c03f947a6f1bf9035edfd579421457241fe2
Juan Carlos <juan.carlos@insparx.com>
2024-05-10 17:21:41 +0200

    chore: Update PrimeNG module imports and add OrderComponent to products module

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -1,19 +1,21 @@
-import { NgModule } from '@angular/core';
-// import { MenuModule } from 'primeng/menu';
-import { MenubarModule } from 'primeng/menubar';
 import { ButtonModule } from 'primeng/button';
-import { Card, CardModule } from 'primeng/card';
+import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
+import { MenubarModule } from 'primeng/menubar';
+import { NgModule } from '@angular/core';
 import { PanelModule } from 'primeng/panel';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
 
 @NgModule({
   exports: [
-    // MenuModule,
     ButtonModule,
     CardModule,
     FieldsetModule,
     MenubarModule,
-    PanelModule
+    PanelModule,
+    TableModule,
+    ToolbarModule,
   ],
 })
 export class PrimeNgModule { }
